2df8000275a94338f11414866a7910eecc6fe58d
Kviatkovskii Sergei <sergejkvyatkovskij@gmail.com>
2023-02-02 13:57:22 +0300

    fixed the issue AB#79378

--- JZCalendarWeekView/JZWeekViewFlowLayout.swift
+++ JZCalendarWeekView/JZWeekViewFlowLayout.swift
@@ -427,7 +427,7 @@ open class JZWeekViewFlowLayout: UICollectionViewFlowLayout {
                 
                 if itemStartTime.hour! <= timelineType.timeRange.lowerBound {
                     startHourY = 0
-                    startMinuteY = 0
+                    startMinuteY = CGFloat(itemStartTime.minute!) * minuteHeight
                 } else {
                     startHourY = CGFloat(itemStartTime.hour!) * hourHeightForZoomLevel
                     startMinuteY = CGFloat(itemStartTime.minute!) * minuteHeight
